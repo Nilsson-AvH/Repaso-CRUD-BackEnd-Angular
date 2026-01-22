3eb76d7df23012afb9fb61e70a4b24c840b43673
Nilsson-AvH <nlr519.com@gmail.com>
2026-01-21 21:51:16 -0500

    [+] npm i mongoose [+] mongo.config.js: Define conecion a MongoDB usando Mongoose [+] index.js Invoca la conexion

---
+++ src/config/mongo.config.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const MONGO_URI = 'mongodb+srv://profe:2OVlIoKuMxmI1FmO@cluster0.lapkq.mongodb.net/db-repaso';
+
+const dbConexion = async () => {
+
+try{
+await mongoose.connect(MONGO_URI,{});
+      console.log('Data base connected')
+}
+catch (error){
+    console.error(error);
+    console.error('Data base connection error :C')
+};
+}
+
+export default dbConexion;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import express from "express";
+import dbConexion from "./config/mongo.config.js";
 import categoriesRoute from "./routes/categories.route.js";
 
 const app = express();
 const port = 3000;
+
+//Conectar a la base de datos
+dbConexion();
 
 //Definir la ruta base a los routers
 app.use('/api/categories', categoriesRoute);
